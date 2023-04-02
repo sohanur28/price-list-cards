0005352665802ec84e65be297a822d7933563009
sohanur28 <gsrahman28@gmail.com>
2023-04-02 19:12:38 +0600

    card

---
+++ src/components/PriceCard/PriceCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PriceCard = ({ price }) => {
+    return (
+        <div>
+            <h2>
+                <span className='text-purple-700 text-5xl font-extrabold'>{price.
+                    price}</span>
+                    <span className='text-2xl'>/mon</span>
+            </h2>
+            <h5 className='text-2xl font-bold'>{price.name}</h5>
+        </div>
+    );
+};
+
+export default PriceCard;
--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PriceCard from '../PriceCard/PriceCard';
 
 const PriceList = () => {
+    const [prices, setPrices] = useState([]);
+
+    useEffect(() => {
+        fetch('prices.json')
+            .then(res => res.json())
+            .then(data => setPrices(data))
+    }, [])
+
+
     return (
         <div>
-            <h2 className='text-5xl text-center bg-purple-300 font-bold p-4'>Awsome Affordable Prices</h2>
+            <h2 className='text-5xl text-center text-purple-950 bg-purple-300 
+            font-bold p-4'>Awsome Affordable Prices</h2>
+            <div className='grid grid-cols-3 gap-3'>
+                {
+                    prices.map(price => <PriceCard
+                        key={price.id}
+                        price={price}
+                    ></PriceCard>)
+                }
+            </div>
         </div>
     );
 };
